a7cc541562f423a0603350d18348c4b990df1128
MokNgai <moyi_001@qq.com>
2020-03-12 20:17:40 +0800

    、、

--- src/xjj.js
+++ src/xjj.js
@@ -1,120 +1,123 @@
-import React, { useState } from "react"
+import React, { useState, Component } from "react"
 import XiaojiejieItem from './xjjItem'
 
-// class Xiaojiejie extends React.Component {
-//     constructor(props){
-//         super(props)
-//         this.state={
-//             inputValue: '',
-//             list: [
-//                 'base',
-//                 'back'
-//             ]
-//         }
-//         this.deleteItem=this.deleteItem.bind(this)
-//     }
+class Xiaojiejie extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            inputValue: '',
+            list: [
+                'base',
+                'back'
+            ]
+        }
+        this.inputChange = this.inputChange.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
+        this.addService = this.addService.bind(this)
+    }
 
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <div>
-//                     <label htmlFor="service" >Add Service: </label>
-//                     <input id="service" className='input' value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
-//                     <button onClick={this.addService.bind(this)}>Add Service</button>
-//                 </div>
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <label htmlFor="service" >Add Service: </label>
+                    <input id="service" className='input' value={this.state.inputValue} onChange={this.inputChange} />
+                    <button onClick={this.addService}>Add Service</button>
+                </div>
 
-//                 <ul>
-//                     {
-//                         this.state.list.map((item, index) => {
-//                                     return (
-//                                             <div>
-//                                                 <XiaojiejieItem 
-//                                                     key = {index + item}
-//                                                     content = {item}
-//                                                     index = {index}
-//                                                     deleteItem = {this.deleteItem}
-//                                                 />
-//                                             </div>
-//                                         )
-//                                 }
-//                             )
-//                     }
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                                    return (
+                                            <div>
+                                                <XiaojiejieItem 
+                                                    key = {index + item}
+                                                    content = {item}
+                                                    index = {index}
+                                                    deleteItem = {this.deleteItem}
+                                                />
+                                            </div>
+                                        )
+                                }
+                            )
+                    }
                     
-//                 </ul>
-//             </React.Fragment>
-//         )
-//     }
-
-//     inputChange (e) {
-//         // console.log(e.target.value)
-//         this.setState({
-//             inputValue: e.target.value
-//         })
-//     }
-
-//     //添加服务
-//     addService () {
-//         this.setState({
-//             list: [...this.state.list, this.state.inputValue],
-//             inputValue: ''
-//         })
-//     }
-
-//     //删除服务
-//     deleteItem (index) {
-//         // console.log(index)
-//         let list = this.state.list
-//         list.splice(index, 1)
-//         this.setState({
-//             list: list
-//         })
-//     }
-// }
-
-
-//将class组件修改为function组件
-function Xiaojiejie (props) {
-    const [inputValue, setInputValue] = useState('')
-    const [list, setList] = useState(['back', 'foot', 'base'])
-
-    const inputChange = function (e) {
-        setInputValue (e.target.value)
+                </ul>
+            </React.Fragment>
+        )
     }
 
-    const addService = function () {
-        setList ([...list, inputValue])
-        setInputValue ('')
+    inputChange (e) {
+        // console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
     }
 
-    const deleteItem = function (index) {
-        const newList = list
-        newList.splice(index, 1)
-        setList (newList)
-        console.log(list)
+    //添加服务
+    addService () {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
     }
 
-    return (
-        <React.Fragment>
-            <div>
-                <label htmlFor="service" >Add Service: </label>
-                <input id="service" className='input' value={inputValue} onChange={inputChange} />
-                <button onClick={addService}>Add Service</button>
-            </div>
-
-            <ul>
-                {list.map((item, index)=>{
-                    return (
-                        <XiaojiejieItem 
-                            key = {index + item}
-                            content = {item}
-                            index = {index}
-                            deleteItem = {deleteItem}
-                        />
-                    )
-                })}
-            </ul>
-        </React.Fragment>
-    )
+    //删除服务
+    deleteItem (index) {
+        // console.log(index)
+        let list = this.state.list
+        list.splice(index, 1)
+        this.setState({
+            list: list
+        })
+        console.log(this.state.list)
+    }
 }
+
+
+// //将class组件修改为function组件
+// function Xiaojiejie (props) {
+//     const [inputValue, setInputValue] = useState('')
+//     const [list, setList] = useState(['back', 'foot', 'base'])
+
+//     const inputChange = function (e) {
+//         setInputValue (e.target.value)
+//     }
+
+//     const addService = function () {
+//         setList ([...list, inputValue])
+//         setInputValue ('')
+//     }
+
+//     const deleteItem = function (index) {
+//         let newList = list.slice()
+//         newList.splice(index, 1)
+//         setList (newList)
+//         console.log(list)
+//     }
+
+//     return (
+//         <React.Fragment>
+//             <div>
+//                 <label htmlFor="service" >Add Service: </label>
+//                 <input id="service" className='input' value={inputValue} onChange={inputChange} />
+//                 <button onClick={addService}>Add Service</button>
+//             </div>
+
+//             <ul>
+//                 {list.map((item, index)=>{
+//                     return (
+//                         <XiaojiejieItem 
+//                             key = {index + item}
+//                             content = {item}
+//                             index = {index}
+//                             deleteItem = {deleteItem}
+//                         />
+//                     )
+//                 })}
+//             </ul>
+//         </React.Fragment>
+//     )
+// }
 
 export default Xiaojiejie
--- src/xjjItem.js
+++ src/xjjItem.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-// class XiaojiejieItem extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.handleClick=this.handleClick.bind(this)
-//     }
-//     render () {
-//         return (
-//             <li onClick = {this.handleClick}>
-//                 {this.props.content}
-//             </li>
-//         )
-//     }
-
-//     handleClick () {
-//         // console.log(this.props.index)
-//         this.props.deleteItem(this.props.index)
-//     }
-// }
-
-//将class组件修改为function组件
-function XiaojiejieItem (props) {
-    const handelClick = function () {
-        props.deleteItem(props.index)
+class XiaojiejieItem extends Component {
+    constructor(props) {
+        super(props)
+        this.handleClick=this.handleClick.bind(this)
+    }
+    render () {
+        return (
+            <li key={this.props.index + this.props.item} onClick = {this.handleClick}>
+                {this.props.content}
+            </li>
+        )
     }
 
-    return(
-        <li key={props.index + props.item} onClick={handelClick}>
-            {props.content}
-        </li>
-    )
+    handleClick () {
+        // console.log(this.props.index)
+        this.props.deleteItem(this.props.index)
+    }
+}
+
+// //将class组件修改为function组件
+// function XiaojiejieItem (props) {
+//     const handelClick = function () {
+//         props.deleteItem(props.index)
+//     }
+
+//     return(
+//         <li key={props.index + props.item} onClick={handelClick}>
+//             {props.content}
+//         </li>
+//     )
+// }
+
+//数据类型校验
+XiaojiejieItem.prototypes = {
+    content: PropTypes.string,
+    index: PropTypes.number,
+    deleteItem: PropTypes.func
 }
 
 export default XiaojiejieItem
